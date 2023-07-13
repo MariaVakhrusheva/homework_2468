67cba82ec07ff6758698a5bc4b179d5c6a82993a
Marya <2510marycat@gmail.com>
2023-07-14 01:03:59 +0500

    четыре задания

---
+++ Task_8/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int evenNum = 0;
+if (number > 1)
+{
+    while (evenNum <= number)
+    {
+       Console.Write(evenNum + ". ");
+        evenNum = evenNum + 2;
+    }
+}
+
+
